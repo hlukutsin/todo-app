47e9818c8dcf0a2816fce17a434b8d34752da190
Hleb Lukutsin <44585107+hlukutsin@users.noreply.github.com>
2024-05-07 19:38:34 +0200

    Add getTodo, updateDodo

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -15,19 +15,45 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    console.log(req);
-    const body = req.body
+    const body = req.body;
 
-    if ((body.name !== undefined && body.text !== undefined){
-
+    if (body.name !== undefined && body.text !== undefined){
+      const todosList = await todos.addTodo({name: body.name, text: body.text});
+      res.send(todosList);
     }
-    // const todosList = await todos.getTodosList();
-    // res.send(todosList);
+
   } catch (e) {
     console.error(e);
   }
 });
 
-    console.log(123)
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    if (id){
+      const todo = await todos.getTodo(id);
+      res.send(todo);
+    }
+
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+
+    if (id){
+      const todo = await todos.updateTodo(id,body);
+      res.send(todo);
+    }
+
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 module.exports = router;
