8d12acef825ac8fdf16e783265984eddb90d771a
gleb99 <44585107+hlukutsin@users.noreply.github.com>
2024-05-09 21:56:43 +0200

    Delete classes

--- backend/services/database.js
+++ backend/services/database.js
@@ -1,4 +1,22 @@
-const Database = require('../classes/Database');
+const MongoClient = require('mongodb').MongoClient;
+
+const dburl = require('../const/index.const');
+
+class Database {
+  constructor() {
+    this.db = null;
+  }
+
+  connectToBase = async () => {
+    try {
+      const client = await MongoClient.connect(dburl);
+      this.db = client.db('todos');
+      console.log('Connected to MongoDB');
+    } catch (err) {
+      console.error('Failed to connect to MongoDB:', err);
+    }
+  };
+}
 
 const dataBase = new Database();
 
