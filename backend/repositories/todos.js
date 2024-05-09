634a5bfbaa6ebf3f39ead4a0539ab6a3183472c5
gleb99 <44585107+hlukutsin@users.noreply.github.com>
2024-05-09 22:36:10 +0200

    Add patch, deleting

--- backend/repositories/todos.js
+++ backend/repositories/todos.js
@@ -18,7 +18,7 @@ class Todos {
       const formattedId = new ObjectId(id);
       return todosCollection.findOne({ _id: formattedId });
     } catch (e) {
-      console.error('Failed to getting Todos list:', e);
+      console.error('Failed to getting Todo:', e);
     }
   };
 
@@ -28,18 +28,48 @@ class Todos {
       await todosCollection.insertOne(data);
       return todosCollection.find().toArray();
     } catch (e) {
-      console.error('Failed to getting Todos list:', e);
+      console.error('Failed to adding Todo:', e);
     }
   };
 
-  updateTodo = async (id, data) => {
+  putTodo = async (id, data) => {
     try {
       const todosCollection = await dataBase.db.collection('todos');
       const formattedId = new ObjectId(id);
       await todosCollection.replaceOne({ _id: formattedId }, data);
       return todosCollection.findOne({ _id: formattedId });
     } catch (e) {
-      console.error('Failed to getting Todos list:', e);
+      console.error('Failed to updating Todo:', e);
+    }
+  };
+
+  patchTodo = async (id, data) => {
+    try {
+      const todosCollection = await dataBase.db.collection('todos');
+      const formattedId = new ObjectId(id);
+      await todosCollection.updateOne({ _id: formattedId }, { $set: data });
+      return todosCollection.findOne({ _id: formattedId });
+    } catch (e) {
+      console.error('Failed to updating Todo:', e);
+    }
+  };
+
+  deleteTodo = async (id) => {
+    try {
+      const todosCollection = await dataBase.db.collection('todos');
+      const formattedId = new ObjectId(id);
+      return todosCollection.deleteOne({ _id: formattedId });
+    } catch (e) {
+      console.error('Failed to deleting Todo:', e);
+    }
+  };
+
+  deleteAll = async () => {
+    try {
+      const todosCollection = await dataBase.db.collection('todos');
+      return todosCollection.deleteMany();
+    } catch (e) {
+      console.error('Failed to deleting Todos list:', e);
     }
   };
 }
--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -17,11 +17,13 @@ router.post('/', async (req, res) => {
   try {
     const body = req.body;
 
-    if (body.name !== undefined && body.text !== undefined){
-      const todosList = await todos.addTodo({name: body.name, text: body.text});
+    if (body.name !== undefined && body.text !== undefined) {
+      const todosList = await todos.addTodo({
+        name: body.name,
+        text: body.text,
+      });
       res.send(todosList);
     }
-
   } catch (e) {
     console.error(e);
   }
@@ -31,11 +33,10 @@ router.get('/:id', async (req, res) => {
   try {
     const id = req.params.id;
 
-    if (id){
+    if (id) {
       const todo = await todos.getTodo(id);
       res.send(todo);
     }
-
   } catch (e) {
     console.error(e);
   }
@@ -46,11 +47,46 @@ router.put('/:id', async (req, res) => {
     const id = req.params.id;
     const body = req.body;
 
-    if (id){
-      const todo = await todos.updateTodo(id,body);
+    if (id) {
+      const todo = await todos.putTodo(id, body);
       res.send(todo);
     }
+  } catch (e) {
+    console.error(e);
+  }
+});
 
+router.patch('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+
+    if (id) {
+      const todo = await todos.patchTodo(id, body);
+      res.send(todo);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    if (id) {
+      const todo = await todos.deleteTodo(id);
+      res.send(todo);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+router.delete('/', async (req, res) => {
+  try {
+    const todo = await todos.deleteAll();
+    res.send(todo);
   } catch (e) {
     console.error(e);
   }
