41114423258dcad8474b4f97fe1c371d77947652
Hleb Lukutsin <44585107+hlukutsin@users.noreply.github.com>
2024-05-16 19:53:15 +0200

    Add base packages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import {Provider} from "react-redux";
+//
+// import { persistor, store } from './store/store.ts';
+
+import App from './App.tsx';
+
+import 'normalize.css';
+// import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+        // <Provider store={store}>
+    <><App /></>
+
+        // </Provider>
+);
